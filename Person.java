29f27457aaee8f2a3cd8a3128af1524bba8d831c
Lauryn <148953403+Laurynlau@users.noreply.github.com>
2023-12-06 19:39:31 +0000

    Add files via upload
    Encapsulation.01

---
+++ Person.java
@@ -0,0 +1,24 @@
+package encapsulationPractice;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	
+	public void setName(String myName) {
+		name = myName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setAge(int myAge) {
+		age = myAge;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+}
